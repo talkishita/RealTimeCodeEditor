fd19177deac4dd8599dbf71c25d7b52c46c147e9
Coders Gyan <codersgyan@Coderss-MacBook-Air.local>
2022-03-04 21:58:06 +0300

    initial commit

---
+++ src/Actions.js
@@ -0,0 +1,12 @@
+const ACTIONS = {
+    JOIN: 'join',
+    JOINED: 'joined',
+    DISCONNECTED: 'disconnected',
+    CODE_CHANGE: 'code-change',
+    SYNC_CODE: 'sync-code',
+    LEAVE: 'leave',
+    OUTPUT_CHANGE: 'output-change',
+    LANGUAGE_CHANGE: 'language-change',
+};
+
+module.exports = ACTIONS;
